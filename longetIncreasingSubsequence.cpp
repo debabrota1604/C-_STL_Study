b32a3bd6ca2e49c2f98291b7de127e92c3db0a4f
DEBABROTA SADHUKHAN <debabrota.sadhukhan3@gmail.com>
2022-03-20 00:36:40 -0400

    daily update

--- longetIncreasingSubsequence.cpp
+++ longetIncreasingSubsequence.cpp
@@ -2,12 +2,17 @@
 #include<map>
 using namespace std;
 int *seq,seqLen;
-map<int, pair<int, int>> m; //pair of index, max_element
+map<pair<int, int>, int> m; //pair of index, max_element
 
 int las(int endIndex){
-    pair<int, int> p; int temp, temp2;
-    int val = las(endIndex -1);
-    
+    pair<int, int> p=make_pair(endIndex,endIndex); int temp, temp2;
+
+    if (endIndex==0) { return 1; }
+    if (m.count(p)) { return m[p]; }
+    else{
+        las(endIndex)
+    }
+
 }
 
 int main(){
@@ -16,7 +21,7 @@ int main(){
     seq = new int[seqLen]; for(int i=0;i<seqLen; i++){ cin >> seq[i]; }
     for(auto i=0; i<seqLen; i++){ cout << seq[i] << " "; }
     
-    cout << "Max Length is: " << las(seqLen) <<endl;
+    cout << "Max Length is: " << las(seqLen-1) <<endl;
 
 
     return 0;
