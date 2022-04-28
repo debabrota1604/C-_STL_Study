2fe6d0263db987457344d7e1a3899806eaddc995
Debabrota <debabrota.sadhukhan3@gmail.com>
2022-04-28 22:35:26 +0530

    daily update

--- leetCode/testFile.cpp
+++ leetCode/testFile.cpp
@@ -1,16 +1,25 @@
 #include<iostream>
-#include<vector>
 using namespace std;
-
-int main(){
-    vector<int> v = {1,2,3,4,5};
-
-    for (auto i=v.begin(); i!=v.end();i=next(i)){
-        cout << *i << " ";
-    }
-
-
-
-
+int count_x(char* p, char x)
+// count the number of occurrences of x in p[]
+// p is assumed to point to a zero-terminated array of char (or to nothing)
+{
+    printf("%u\n",&p);
+    if (p==nullptr)
     return 0;
+    int count = 0;
+    for (; *p!=0; ++p)
+        if (*p==x){ 
+            ++count;
+            *p = 'f';
+        }
+return count;
+}
+int main(){ 
+    char *s = "abxyxe";
+    printf("%u\n",&s);
+    // cout << *s <<endl;
+    cout << count_x(s,'x')<<endl;
+    cout << s <<endl;
+
 }
