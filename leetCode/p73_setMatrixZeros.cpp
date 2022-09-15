93e8b960e7fec246da3216b1e0141acf84938c66
Debabrota Sadhukhan <debabrota.sadhukhan3@gmail.com>
2022-09-15 20:19:15 +0530

    msg

---
+++ leetCode/p73_setMatrixZeros.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<bool> row (matrix.size(), false), col(matrix[0].size(), false);
+        for(int i=0; i<matrix.size(); i++){
+            for(int j=0; j<matrix[0].size(); j++){
+                p.emplace_back(make_pair(i,j));
+            }
+        }
+        for(int i=0; i<)
+    }
+};
+
+int main(){
+
+}
---
+++ leetCode/testFile.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int count_x(char* p, char x)
+// count the number of occurrences of x in p[]
+// p is assumed to point to a zero-terminated array of char (or to nothing)
+{
+    printf("%u\n",&p);
+    if (p==nullptr)
+    return 0;
+    int count = 0;
+    for (; *p!=0; ++p)
+        if (*p==x){ 
+            ++count;
+            *p = 'f';
+        }
+return count;
+}
+int main(){ 
+    char *s = "abxyxe";
+    printf("%u\n",&s);
+    // cout << *s <<endl;
+    cout << count_x(s,'x')<<endl;
+    cout << s <<endl;
+
+
+    vector< vector< bool > > v( (10, false),false);
+
+}
