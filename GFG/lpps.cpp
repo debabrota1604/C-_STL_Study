531292c07730a672a8a5fdf0d23125dacfb84d31
Debabrota Sadhukhan <debabrota.sadhukhan3@gmail.com>
2023-02-08 17:22:58 +0530

    Old updated files

--- GFG/lpps.cpp
+++ GFG/lpps.cpp
@@ -1,6 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> computeLPS(string pattern){
+    int len=0; vector<int> lppsArr (pattern.size(), 0);
+    int iter = 1; //lppsArr[0] is always zero
+    while(iter <pattern.size()){
+        if(pattern[iter] == pattern[len]){ len++; lppsArr[iter]=len; iter++; }
+        else{
+            if(len !=0) len = lppsArr[len-1]; //Example: AAACAAAA and iter= 7
+            else {lppsArr[iter] = 0; iter++;}
+        }
+    }
+    return lppsArr;
+}
+
 vector<int> computeLPPS(string pattern){
     int len=0; vector<int> lppsArr (pattern.size(), 0);
     int iter = 1; //lppsArr[0] is always zero
@@ -8,7 +21,7 @@ vector<int> computeLPPS(string pattern){
         if(pattern[iter] == pattern[len]){ len++; lppsArr[iter]=len; iter++; }
         else{
             if(len !=0) len = lppsArr[len-1]; //Example: AAACAAAA and iter= 7
-            else lppsArr[iter++] = 0;
+            else {lppsArr[iter] = 0; iter++;}
         }
     }
     return lppsArr;
